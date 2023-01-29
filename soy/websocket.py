106bd3fbe2cc8dbe0ce55cc69e83f5a74f7654cb
ryandkutz <ryandkutz@gmail.com>
2023-01-29 00:18:50 -0600

    set up database updates for live tweets

--- soy/websocket.py
+++ soy/websocket.py
@@ -2,16 +2,16 @@ from core.token import get_token
 from core.livesource import LiveSource
 import core.db as db
 from dotenv import load_dotenv
-from classifier import classify_tweet
+from classifier import process_tweet
 
 def main():
     load_dotenv()
-    db.create_connection()
+    connection = db.create_connection()
     token = get_token()
     ls = LiveSource(token)
     # https://developer.twitter.com/en/docs/twitter-api/tweets/filtered-stream/integrate/build-a-rule
-    ls.add_rule(" -is:retweet -from:elonmusk (Elon Musk OR #ElonMusk OR @elonmusk)", "Elon Musk")
-    ls.start(classify_tweet)
+    ls.add_rule("-is:retweet -from:elonmusk (Elon Musk OR #ElonMusk OR @elonmusk)", "Elon Musk")
+    ls.start(process_tweet,connection)
 
 if __name__ == "__main__":
     main()
